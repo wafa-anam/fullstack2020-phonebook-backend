0e2768282d0e319eb25dc22c4f8ed56437399677
Wafa Anam <wafa.anam@symend.com>
2020-05-05 22:09:40 -0600

    uibuild

---
+++ build/precache-manifest.6a3a0c63b865991ddae54f61efce9d9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f6ab060f57528b846ca2be60d6f0c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33ff241adb91c386ab25",
+    "url": "/static/css/main.c7ea9c59.chunk.css"
+  },
+  {
+    "revision": "c45d04b9c957ba678cf8",
+    "url": "/static/js/2.c6b8127b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c6b8127b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33ff241adb91c386ab25",
+    "url": "/static/js/main.fbace059.chunk.js"
+  },
+  {
+    "revision": "bccee6d650832870a352",
+    "url": "/static/js/runtime-main.5401d62a.js"
+  }
+]);
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
     {
